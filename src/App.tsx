94d89e23a271a584992f9516efab508ec209c0ee
jst-gunji <go.gunji@stream.co.jp>
2022-06-16 20:43:42 +0900

    取得映画リストごとにRowコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { RowComponent } from "./components/Row/Row";
+import { requestMovies } from "./utils/requestMovies";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RowComponent
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requestMovies.feachNetflixOriginals}
+        isLargeRow
+      />
+      <RowComponent title="Top Rated" fetchUrl={requestMovies.feactTopRated} />
+      <RowComponent
+        title="Action Movies"
+        fetchUrl={requestMovies.feactActionMovies}
+      />
+      <RowComponent
+        title="Comedy Movies"
+        fetchUrl={requestMovies.feactComedyMovies}
+      />
+      <RowComponent
+        title="Horror Movies"
+        fetchUrl={requestMovies.feactHorrorMovies}
+      />
+      <RowComponent
+        title="Romance Movies"
+        fetchUrl={requestMovies.feactRomanceMovies}
+      />
+      <RowComponent
+        title="DOcumentaries"
+        fetchUrl={requestMovies.feactDocumentMovies}
+      />
+    </div>
+  );
 }
 
 export default App;
